1eaeb97060b53ba0efebbfbcf279a9d0551ed2ea
Tom Ford <tfordy63@gmail.com>
2025-08-27 22:57:41 +0200

    only this?

--- machines/ubuntuwsl.nix
+++ machines/ubuntuwsl.nix
@@ -3,19 +3,5 @@
   pkgs,
   ...
 }: {
-  # system.stateVersion = 6;
-
-  # ids.gids.nixbld = 350;
-
   nixpkgs.config.allowUnfree = true;
-
-  nix = {
-    enable = true;
-    settings.experimental-features = ["nix-command" "flakes"];
-  };
-
-  environment.shells = with pkgs; [bashInteractive];
-  environment.systemPackages = with pkgs; [
-    cachix
-  ];
 }
