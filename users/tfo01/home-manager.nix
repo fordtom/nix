6df57e2c65be48458dbeb9b8bd0032397e7a09cf
Tom Ford <tfordy63@gmail.com>
2025-09-01 08:37:09 +0200

    path fix

--- users/tfo01/home-manager.nix
+++ users/tfo01/home-manager.nix
@@ -96,7 +96,7 @@ in {
     ];
 
     extraConfig = {
-      credential.helper = "/mnt/c/Program Files/Git/mingw64/bin/git-credential-manager.exe";
+      credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
     };
   };
 
