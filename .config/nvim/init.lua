c1d286d520f98c291e39eda6d3b00cb9702ac237
Tom Ford <t@tomrford.com>
2026-06-07 13:36:18 +0200

    a bit more tidying

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,12 @@
-require("config")
+-- Set leader before loading plugins
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Install and configure plugins
+require("config.plugins")
+
+-- Load options and keymaps
+require("config.options")
+require("config.treesitter")
+require("config.terminal_colors")
+require("config.keymaps")
--- .config/nvim/lua/config/plugins/init.lua
+++ .config/nvim/lua/config/plugins/init.lua
@@ -7,8 +7,24 @@ vim.pack.add({
   { src = "https://github.com/tpope/vim-sleuth" },
 })
 
-require("config.plugins.icons")
+require("mini.icons").setup()
+
 require("config.plugins.snacks")
 require("config.plugins.lualine")
 require("config.plugins.lsp")
-require("config.plugins.blink")
+
+require("blink.cmp").setup({
+  keymap = { preset = "default" },
+  appearance = {
+    nerd_font_variant = "mono",
+  },
+  completion = {
+    documentation = { auto_show = false },
+  },
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer" },
+  },
+  fuzzy = {
+    implementation = "prefer_rust_with_warning",
+  },
+})
